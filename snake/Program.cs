807bfe826b303d3ca2ebb7820b4f3373baf5ae49
Alexandr Dukhno <a.dukhno1205@gmail.com>
2016-10-08 16:55:19 +0300

    Added function Draw()

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,6 +10,23 @@ namespace snake
     {
         static void Main(string[] args)
         {
+            int x = 2;
+            int y = 3;
+            char symb = 'X';
+
+            Draw(x, y, symb);
+
+            int x1 = 20;
+            int y1 = 20;
+
+            Draw(x1, y1, symb);
+
+            Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym) {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
